7e17ffd37185cc8e5ed7ccd90ca4c41a51cadbd3
steventhomasmiller <steventhomasmiller@gmail.com>
2015-01-14 12:25:16 -0800

    All's working except the playAgain()

--- Assignment2/Program.java
+++ Assignment2/Program.java
@@ -1,34 +1,35 @@
 package com.spconger.Assignment2;
-
+import java.util.Random;
 import java.util.Scanner;
-import java.util.Scanner;
-
-int triesNumber = 0;
-int guess;
-boolean correct = false;
 
 public class Program {
-
-	Scanner scan = new Scanner(Systerm.in);
+	
+	Scanner scan = new Scanner(System.in);
+	
+	int triesNumber = 0;
+	int guess;
+	String play;
+	boolean correct = false;
 
 	public static void main(String[] args) {
 		Program p = new Program();
 		p.guessingGame();
 	}
-
+	
 	private int getRandom()
 	{
 		Random r = new Random();
 		int number=r.nextInt(1000);
 		return number;
 	}
-
+	
 	private void guessingGame()
 	{
+		System.out.println("Guess a number between 1 and 1000");
+		
 		do
 		{
-			System.out.println("Guess a number between 1 and 1000");
-			guess = input.nextInt();
+			guess = scan.nextInt();
 			triesNumber++;
 			int guessNumber = getRandom();
 
@@ -38,6 +39,7 @@ public class Program {
 				System.out.println("That is the number.");
 				System.out.println("The number was" + guessNumber);
 				System.out.println("Play again? Y/N");
+//				String play = nextString.playAgain(); //not sure this is right
 
 			}
 			else if (guess < guessNumber)
@@ -46,14 +48,23 @@ public class Program {
 			}
 			else if (guess > guessNumber)
 			{
-				System.out.println("Your guess is too high.")
+				System.out.println("Your guess is too high.");
 			}
-		} while (correct == false && triesNumber <= 10)
+		} while (correct == false && triesNumber <= 10);
+		System.out.println("You've exceeded the acceptable number of guesses.");
 		
-			// System.out.println("Sorry, you've exceeded the permitted number of guesses. You can no longer play this excellent game.");
-			// System.out.println("Go home now.");
-
 	}
+	
+//	private void playAgain()
+//	{
+//		if (play == "y" || "yes")
+//		{
+//			guessingGame();
+//		}
+//		else
+//		{
+//			System.out.println("Very well, then. You may leave.");
+//		}
+//	}
 
 }
-

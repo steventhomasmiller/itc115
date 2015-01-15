af2eb9d3621ac450d9bb927edffa238778d65f00
steventhomasmiller <steventhomasmiller@gmail.com>
2015-01-14 18:01:27 -0800

    Create Program.java

---
+++ VehicleExample/Program.java
@@ -0,0 +1,12 @@
+package com.spconger.VehicleExample;
+
+public class Program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Vehicle v = new Vehicle();
+		v.setPassengers(5);
+		System.out.println("The car holds " + v.getPassengers() + " passengers.");
+	}
+
+}

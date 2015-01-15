e90bc536104618ad65204a90978c797cf26a883b
steventhomasmiller <steventhomasmiller@gmail.com>
2015-01-14 18:22:03 -0800

    Added some stuff.

--- VehicleExample/Display.java
+++ VehicleExample/Display.java
@@ -11,11 +11,22 @@ public class Display {
 	{
 		vehicle = new Vehicle();
 		scan = new Scanner(System.in);
+		getInputs();
+		getOutput();
 	}
 	
 	private void getInputs()
 	{
-		System.out.println("Enter seating capacity of vehicle");
-		
+		System.out.println("Enter seating capacity of vehicle: ");
+		vehicle.setPassengers(scan.nextInt());
+		System.out.println("Enter the fuel capacity: ");
+		vehicle.setFuelCapacity(scan.nextInt());
+		System.out.println("Enter the mpg: ");
+		vehicle.setMpg(scan.nextInt());
+	}
+	
+	private void getOutput()
+	{
+		System.out.println("Your vehicle can hold " + vehicle.getPassengers() + " passengers, and has a range of " + vehicle.calculateRange() + "miles!!");
 	}
 }

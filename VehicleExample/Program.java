e6456194350882074976b93842bad2c625cbf2ce
steventhomasmiller <steventhomasmiller@gmail.com>
2015-01-14 18:22:34 -0800

    Deleted some stuff.

--- VehicleExample/Program.java
+++ VehicleExample/Program.java
@@ -3,10 +3,7 @@ package com.spconger.VehicleExample;
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Vehicle v = new Vehicle();
-		v.setPassengers(5);
-		System.out.println("The car holds " + v.getPassengers() + " passengers.");
+		Display d = new Display(); //calls constructor
 	}
 
 }

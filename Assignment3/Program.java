dcf0f29273ccc21fefade2e01333d28c5d74a2ac
steventhomasmiller <steventhomasmiller@gmail.com>
2015-01-21 17:56:07 -0800

    w/constructor

--- Assignment3/Program.java
+++ Assignment3/Program.java
@@ -3,7 +3,7 @@ package com.spconger.Assignment3;
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Display d = new Display(); //calls constructor
 
 	}
 

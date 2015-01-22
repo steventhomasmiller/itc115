90571555bb098422a46210985a7403ef0fba8b04
steventhomasmiller <steventhomasmiller@gmail.com>
2015-01-22 14:27:55 -0800

    Calculations working.

--- Assignment3/Stats.java
+++ Assignment3/Stats.java
@@ -4,63 +4,63 @@ public class Stats {
 	
 	//private fields
 	
-	private int numTotalBats;
-	private int numHits;
-	private int numOuts;
-	private int numWalks;
+		private int numTotalBats;
+		private int numHits;
+		private int numOuts;
+		private int numWalks;
 
-	//public getters and setters
-	//accessors and mutators
-	
-	public int getNumTotalBats()
-	{
-		return numTotalBats;
-	}
-	
-	public void setNumTotalBats(int numTotalBats)
-	{
-		this.numTotalBats = numTotalBats;
-	}
-	
-	public int getNumHits()
-	{
-		return numHits;
-	}
-	
-	public void setNumHits(int numHits)
-	{
-		this.numHits = numHits;
-	}
-	
-	public int getNumOuts()
-	{
-		return numOuts;
-	}
-	
-	public void setNumOuts(int numOuts)
-	{
-		this.numOuts = numOuts;
-	}
-	
-	public int getNumWalks()
-	{
-		return numWalks;
-	}
-	
-	public void setNumWalks(int numWalks)
-	{
-		this.numWalks = numWalks;
-	}
-	
-	//public methods
-	public double calculateBattingAverage()
-	{
-		return getNumHits() / getNumTotalBats();
-	}
-	
-	public int calculateOnBasePercentage()
-	{
-		return getNumHits() + getNumWalks() / getNumTotalBats(); 
-	}
+		//public getters and setters
+		//accessors and mutators
+		
+		public int getNumTotalBats()
+		{
+			return numTotalBats;
+		}
+		
+		public void setNumTotalBats(int numTotalBats)
+		{
+			this.numTotalBats = numTotalBats;
+		}
+		
+		public int getNumHits()
+		{
+			return numHits;
+		}
+		
+		public void setNumHits(int numHits)
+		{
+			this.numHits = numHits;
+		}
+		
+		public int getNumOuts()
+		{
+			return numOuts;
+		}
+		
+		public void setNumOuts(int numOuts)
+		{
+			this.numOuts = numOuts;
+		}
+		
+		public int getNumWalks()
+		{
+			return numWalks;
+		}
+		
+		public void setNumWalks(int numWalks)
+		{
+			this.numWalks = numWalks;
+		}
+		
+		//public methods
+		public double calculateBattingAverage()
+		{
+			return 1.0 * getNumHits() / getNumTotalBats();
+		}
+		
+		public double calculateOnBasePercentage()
+		{
+			return 1.0 * (getNumHits() + getNumWalks()) / getNumTotalBats(); 
+		}
 
 }

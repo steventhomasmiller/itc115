3befd07a8bccb71c1ecd2a63f99031dfe4f29b5d
steventhomasmiller <steventhomasmiller@gmail.com>
2015-01-22 14:46:31 -0800

    w/documentation

--- Assignment3/Stats.java
+++ Assignment3/Stats.java
@@ -2,6 +2,10 @@ package com.spconger.Assignment3;
 
 public class Stats {
 	
+	/* This class gets player's number of hits, walks, outs and at bats.
+	 * It also calculates batting average and on-base percentage
+	 * Steve Miller, 1/21/15 */
+	
 	//private fields
 	
 		private int numTotalBats;
@@ -53,14 +57,14 @@ public class Stats {
 		}
 		
 		//public methods
-		public double calculateBattingAverage()
+		public double calculateBattingAverage() //double because it's a decimal
 		{
-			return 1.0 * getNumHits() / getNumTotalBats();
+			return 1.0 * getNumHits() / getNumTotalBats(); //multiply by 1.0 to make percentage
 		}
 		
-		public double calculateOnBasePercentage()
+		public double calculateOnBasePercentage() //same as above
 		{
-			return 1.0 * (getNumHits() + getNumWalks()) / getNumTotalBats(); 
+			return 1.0 * (getNumHits() + getNumWalks()) / getNumTotalBats(); //same as above
 		}
 
 }

446aa530d99f4d661809ebfd6b02ec6057e46abc
steventhomasmiller <steventhomasmiller@gmail.com>
2015-01-21 17:55:32 -0800

    Full display

--- Assignment3/Display.java
+++ Assignment3/Display.java
@@ -1,5 +1,36 @@
 package com.spconger.Assignment3;
 
+import java.util.Scanner;
+
 public class Display {
 
+	private Stats stats;
+	Scanner scan;
+	
+	public Display()
+	{
+		stats = new Stats();
+		scan = new Scanner(System.in);
+		getInputs();
+		getOutputs();
+	}
+	
+	private void getInputs()
+	{
+		System.out.println("Enter player's total at bats: ");
+		stats.setNumTotalBats(scan.nextInt());
+		System.out.println("Enter player's total hits: ");
+		stats.setNumHits(scan.nextInt());
+		System.out.println("Enter player's total walks: ");
+		stats.setNumWalks(scan.nextInt());
+		System.out.println("Enter player's total outs: ");
+		stats.setNumOuts(scan.nextInt());
+	}
+	
+	private void getOutputs()
+	{
+		System.out.println("The player's batting average is " + stats.calculateBattingAverage()+ ", and their on-base percentage is " + stats.calculateOnBasePercentage() + ".");
+	}
 }
+
+

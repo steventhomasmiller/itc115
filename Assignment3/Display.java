32d3e6ef1ff94b875a930549e25bda217ca5ce85
steventhomasmiller <steventhomasmiller@gmail.com>
2015-01-22 14:47:42 -0800

    w/documentation

--- Assignment3/Display.java
+++ Assignment3/Display.java
@@ -3,19 +3,23 @@ package com.spconger.Assignment3;
 import java.util.Scanner;
 
 public class Display {
-
+	
+	/* This class prompts the user to enter the player's information.
+	 * It also displays the player's batting average and on-base percentage
+	 * Steve Miller. 1/21/15  */
+	
 	private Stats stats;
 	Scanner scan;
 	
-	public Display()
+	public Display() //The constructor, called in the main
 	{
-		stats = new Stats();
+		stats = new Stats(); //must match class name
 		scan = new Scanner(System.in);
-		getInputs();
-		getOutputs();
+		getInputs(); //called below
+		getOutputs(); //also called below
 	}
 	
-	private void getInputs()
+	private void getInputs() //prompts user for information
 	{
 		System.out.println("Enter player's total at bats: ");
 		stats.setNumTotalBats(scan.nextInt());
@@ -27,10 +31,9 @@ public class Display {
 		stats.setNumOuts(scan.nextInt());
 	}
 	
-	private void getOutputs()
+	private void getOutputs() //displays calculations
 	{
 		System.out.println("The player's batting average is " + stats.calculateBattingAverage()+ ", and their on-base percentage is " + stats.calculateOnBasePercentage() + ".");
 	}
+
 }
-
-

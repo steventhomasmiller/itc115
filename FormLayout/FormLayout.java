858f8ddf993afb00d5ec394baf5f109dd204e146
steventhomasmiller <steventhomasmiller@gmail.com>
2015-01-26 18:55:12 -0800

    Works

--- FormLayout/FormLayout.java
+++ FormLayout/FormLayout.java
@@ -1,5 +1,7 @@
 package com.spconger.FormLayout;
 
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -12,11 +14,18 @@ import javax.swing.JTextField;
 
 public class FormLayout {
 	JFrame frame;
+	
 	JPanel panel1;
+	JPanel borderPanel;
+	JPanel buttonPanel;
+	
 	JLabel label1;
 	JLabel label2;
+	
 	JTextField textName;
+	
 	JButton button;
+	JButton exitButton;
 	
 	public FormLayout() //constructor
 	{
@@ -29,24 +38,45 @@ public class FormLayout {
 		frame = new JFrame();
 		frame.setBounds(100, 100, 400, 200);
 		frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
-		frame.add(panel1);
+		frame.add(createBorderPanel());
 		frame.setVisible(true);
 	}
 	
-	private void createPanel()
+	private JPanel createBorderPanel()
+	{
+		borderPanel = new JPanel();
+		borderPanel.setLayout(new BorderLayout());
+		borderPanel.add(createPanel(), BorderLayout.NORTH);
+		borderPanel.add(createButtonPanel(), BorderLayout.SOUTH);
+		return borderPanel;
+	}
+	
+	private JPanel createButtonPanel()
+	{
+		buttonPanel = new JPanel();
+		buttonPanel.setLayout(new FlowLayout());
+		button = new JButton("Click");
+		button.addActionListener(new ButtonListener());
+		exitButton = new JButton("Exit");
+		exitButton.addActionListener(new ExitListener()); //closes form and ends program upon click
+		buttonPanel.add(button);
+		buttonPanel.add(exitButton);
+		return buttonPanel;
+	}
+	
+	private JPanel createPanel()
 	{
 		panel1 = new JPanel();
 		panel1.setLayout(new GridLayout(2,2));
 		label1 = new JLabel("Enter your name: ");
 		textName = new JTextField(25);
-		button = new JButton("Click");
-		button.addActionListener(new ButtonListener());
 		
 		label2 = new JLabel();
 		panel1.add(label1);
 		panel1.add(textName);
-		panel1.add(button);
 		panel1.add(label2);
+		
+		return panel1;
 	}
 	
 	private class ButtonListener implements ActionListener
@@ -55,7 +85,19 @@ public class FormLayout {
 		@Override
 		public void actionPerformed(ActionEvent e) 
 		{
-			label2.setText("Hello " + textName.getText());
+			label2.setText("Hello, " + textName.getText());
+		}
+		
+	}
+	
+	private class ExitListener implements ActionListener
+	{
+
+		@Override
+		public void actionPerformed(ActionEvent e) 
+		{
+			System.exit(0);
+			
 		}
 		
 	}

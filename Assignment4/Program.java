0b68dfd3a91d789f1606a1d145aed68fbc7a3fd0
steventhomasmiller <steventhomasmiller@gmail.com>
2015-02-02 18:47:44 -0800

    Update Program.java

--- Assignment4/Program.java
+++ Assignment4/Program.java
@@ -4,7 +4,9 @@ public class Program {
 
 	public static void main(String[] args) 
 	{
-		CardForm cf = new CardForm();
+		//CardForm cf = new CardForm();
+		Deck myCoolDeckYo = new Deck();
+		myCoolDeckYo.getCards();
 	}
 
 }

9a4db772453dd2637d11b1c1e0f5d84032e89fc3
steventhomasmiller <steventhomasmiller@gmail.com>
2015-02-06 12:55:56 -0800

    Update Deck.java

--- Assignment4/Deck.java
+++ Assignment4/Deck.java
@@ -1,41 +1,18 @@
-package com.spconger.Assignment4;
+package com.spconger.Assignment4a;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Random;
 
-
-public class Deck {
+public class Deck 
+{
+	final String Suits[] = { "hearts", "spades", "diamonds", "clubs" };
+	private String Ranks[] = { "Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King" };
 	
 	private ArrayList<Card> cards;
 	
-	public Deck()
+	public cardDeck()
 	{
 		cards = new ArrayList<Card>();
-		String[] suits = new String[]{"club","diamond","heart","spade"};
-		String[] ranks = new String[]{"Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"};//write them out
-		
-		for(int i=0; i<suits.length; i++)
-		{
-			Card c = new Card();
-			c.setSuit(suits [i]);
-			for(int j = 0; j<ranks.length; j++)
-			{
-				c.setRank(ranks [j]);
-				cards.add(c);
-				System.out.print(cards.get(i).getSuit()+ ", "+cards.get(i).getRank());
-			}
-		}
 	}
 	
-	public void addCard(Card c)
-	{
-		cards.add(c);
-	}
-	
-	public ArrayList<Card> getCards()
-	{
-		return cards;
-	}
-	
-} 
+}

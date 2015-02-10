ed9b3e63ebedc6bed635bea15e0b3a493a4ffd9a
steventhomasmiller <steventhomasmiller@gmail.com>
2015-02-09 18:03:17 -0800

    Update Program.java

--- InheritanceExample/Program.java
+++ InheritanceExample/Program.java
@@ -5,6 +5,14 @@ public class Program {
 	public static void main(String[] args) 
 	{
 		Customer c = new Customer();
+		c.setName("Steve");
+		c.setEmail("Steve@spconger.com");
+		c.setPhone("2065555551"); //comes from Person class
+		c.setZipCode("98122"); //ibid
+		c.setRegistrationDate("2/9/2015");//ibid
+		
+		System.out.println(c.toString());
 	}
 
 }
+

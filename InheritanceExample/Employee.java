0a1aeef07ce5b5f55d1835e4cba034cffee4d69e
steventhomasmiller <steventhomasmiller@gmail.com>
2015-02-11 16:59:06 -0800

    Update Employee.java

--- InheritanceExample/Employee.java
+++ InheritanceExample/Employee.java
@@ -1,47 +1,39 @@
 package com.spconger.InheritanceExample;
 
-public abstract class Employee extends Person
-{
+public abstract class Employee extends Person {
+	//private fields
 	private int employeeID;
 	private String hireDate;
 	private String dept;
 	
-	public int getEmployeeID() 
-	{
+	//public getters and setters
+	public int getEmployeeID() {
 		return employeeID;
 	}
-	
-	public void setEmployeeID(int employeeID) 
-	{
+	public void setEmployeeID(int employeeID) {
 		this.employeeID = employeeID;
 	}
-	
-	public String getHireDate() 
-	{
+	public String getHireDate() {
 		return hireDate;
 	}
-	
-	public void setHireDate(String hireDate) 
-	{
+	public void setHireDate(String hireDate) {
 		this.hireDate = hireDate;
 	}
-	
-	public String getDept() 
-	{
+	public String getDept() {
 		return dept;
 	}
-	
-	public void setDept(String dept) 
-	{
+	public void setDept(String dept) {
 		this.dept = dept;
 	}
+
+	//public method
+	public abstract double calculateMonthlyPay();
 	
-	//public method not to be implemented
-	public abstract double calculateMontlyPay(); //method signature
-	
-	public String toString()
-	{
+	public String toString(){
 		String parentString = super.toString();
-		return Integer.toString(getEmployeeID()) + ", " + getHireDate() + " " + getDept() + parentString; //converting integer to string
+		return Integer.toString(getEmployeeID()) + ", "
+				+ getHireDate() + ", " + getDept() + parentString;
 	}
+	
+	
 }

8693afa9e19a203a1370a406aeaa2f26f5297134
steventhomasmiller <steventhomasmiller@gmail.com>
2015-02-11 17:29:12 -0800

    Create Bulk.java

---
+++ GroceryStore/Bulk.java
@@ -0,0 +1,30 @@
+package com.spconger.GroceryStore;
+
+public class Bulk extends Item
+{
+	private double weight;
+	private int bin;
+
+	public double getWeight() 
+	{
+		return weight;
+	}
+
+	public void setWeight(double weight) 
+	{
+		this.weight = weight;
+	}
+
+	public int getBin() 
+	{
+		return bin;
+	}
+
+	public void setBin(int bin) 
+	{
+		this.bin = bin;
+	}
+
+	
+	
+}

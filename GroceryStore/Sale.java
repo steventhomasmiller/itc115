dd59407cf80cfd16cb4476ada5b4f77682ea89e5
steventhomasmiller <steventhomasmiller@gmail.com>
2015-02-11 17:57:39 -0800

    Update Sale.java

--- GroceryStore/Sale.java
+++ GroceryStore/Sale.java
@@ -2,7 +2,7 @@ package com.spconger.GroceryStore;
 
 import java.util.ArrayList;
 
-public class Sale 
+public class Sale implements ListOperations 
 {
 	private ArrayList<Item> basket = new ArrayList<Item>();
 	private final double TAXPERCENT=.095;
@@ -20,5 +20,46 @@ public class Sale
 	public double calculateTax()
 	{
 		
+	}
+
+	@Override
+	public void add(Item i) 
+	{
+		basket.add(i);
+		
+//		if(i.getClass().equals("Bulk"))
+//		{
+//			Bulk bi = (Bulk)i;
+//			basket.add(i);
+//		}
+//		else
+//		{
+//			PrePackage p = (PrePackage)i;
+//			basket.add(p);
+//		}
+	}
+
+	@Override
+	public void edit(Item i) 
+	{
+		for(Item it : basket)
+		{
+			if (it.getSku()==i.getSku())
+			{
+				it=i;
+			}
+		}
+	}
+
+	@Override
+	public void remove(Item i) 
+	{
+		for(Item it : basket)
+		{
+			if (it.getSku()==i.getSku())
+			{
+				basket.remove(it);
+			}
+		}
 	}
 }

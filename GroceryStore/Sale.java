21212811caa7749ffb55c71e3321fbc1c0f89ffd
steventhomasmiller <steventhomasmiller@gmail.com>
2015-02-11 17:39:46 -0800

    Create Sale.java

---
+++ GroceryStore/Sale.java
@@ -0,0 +1,24 @@
+package com.spconger.GroceryStore;
+
+import java.util.ArrayList;
+
+public class Sale 
+{
+	private ArrayList<Item> basket = new ArrayList<Item>();
+	private final double TAXPERCENT=.095;
+	
+	public double getTotalPrice()
+	{
+		
+	}
+	
+	public ArrayList<Item> getItems()
+	{
+		
+	}
+	
+	public double calculateTax()
+	{
+		
+	}
+}

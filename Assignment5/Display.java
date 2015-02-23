44e7e428831a566ee915103b710f5aeab6baca2f
steventhomasmiller <steventhomasmiller@gmail.com>
2015-02-22 19:44:06 -0800

    Update Display.java

--- Assignment5/Display.java
+++ Assignment5/Display.java
@@ -5,6 +5,7 @@ import java.awt.FlowLayout;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.IOException;
 import java.util.ArrayList;
 
 import javax.swing.DefaultListModel;
@@ -14,11 +15,12 @@ import javax.swing.JLabel;
 import javax.swing.JList;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
 public class Display 
 {
-	private ReadFile file;
+	final String FILEPATH ="C:\\temp\\textfile.txt";
 	
 	private JFrame frame;
 	private JPanel borderPanel;
@@ -26,6 +28,9 @@ public class Display
 	private JPanel buttonPanel;
 	private JScrollPane scrollPane;
 	private JLabel greetingPrompt;
+	private JTextArea textArea;
+	private JButton openButton;
+	private JButton saveButton;
 	private JButton exitButton;
 	
 	public Display()
@@ -47,7 +52,7 @@ public class Display
 		borderPanel = new JPanel();
 		borderPanel.setLayout(new BorderLayout());
 		borderPanel.add(createGreetingPanel(), BorderLayout.NORTH);
-		borderPanel.add(createScrollPane(), BorderLayout.CENTER);
+		borderPanel.add(createButtonPanel(), BorderLayout.CENTER);
 		borderPanel.add(createButtonPanel(), BorderLayout.SOUTH);
 		return borderPanel;
 	}
@@ -62,5 +67,83 @@ public class Display
 	}
 	
 	
+	private JScrollPane createTextArea()
+	{
+		textArea = new JTextArea();
+		scrollPane = new JScrollPane(textArea);
+		scrollPane.setBounds(50, 50, 400, 400);
+		return scrollPane;
+	}
 	
+	private JPanel createButtonPanel()
+	{
+		buttonPanel = new JPanel();
+		buttonPanel.setLayout(new FlowLayout());
+		
+		openButton = new JButton("Open");
+		openButton.addActionListener(new OpenButtonListener());
+		
+		saveButton = new JButton ("Save");
+		saveButton.addActionListener(new SaveButtonListener());
+		
+		exitButton = new JButton("Exit");
+		exitButton.addActionListener(new ExitButtonListener());
+		
+		buttonPanel.add(openButton);
+		buttonPanel.add(saveButton);
+		buttonPanel.add(exitButton);
+		
+		return buttonPanel;		
+	}
+	
+	private class OpenButtonListener implements ActionListener
+	{
+		@Override
+		public void actionPerformed(ActionEvent e)
+		{
+			ReadFile rf = new ReadFile(FILEPATH);
+			String content;
+			
+			try
+			{
+				content = rf.getText();
+				
+				textArea.setText(content);
+			}
+			catch (IOException err)
+			{
+				textArea.setText(err.getMessage());
+			}
+		}
+	}
+	
+	private class SaveButtonListener implements ActionListener
+	{
+		@Override
+		public void actionPerformed(ActionEvent e)
+		{
+			try
+			{
+				String textInBox = textArea.getText();
+				
+				WriteFile wf = new WriteFile(FILEPATH);
+				wf.addText(textInBox);
+				
+				wf.closeFile();
+			}
+			catch (IOException err)
+			{
+				textArea.setText(err.getMessage());
+			}
+		}
+	}
+	
+	private class ExitButtonListener implements ActionListener
+	{
+		@Override
+		public void actionPerformed(ActionEvent e)
+		{
+			System.exit(0);
+		}
+	}
 }

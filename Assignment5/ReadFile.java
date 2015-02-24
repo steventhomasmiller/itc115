e9c27a57fcfcc87c45f57e3c5788426aeea5452d
steventhomasmiller <steventhomasmiller@gmail.com>
2015-02-24 08:29:21 -0800

    w/documentation

--- Assignment5/ReadFile.java
+++ Assignment5/ReadFile.java
@@ -8,20 +8,25 @@ import java.io.InputStreamReader;
 
 public class ReadFile 
 {
+	/* This class allows the user to pull text from a text file.
+	 * Steve Miller, 2/13/15
+	*/
+	
 	private String path;
 	
-	public ReadFile(String path)
+	public ReadFile(String path) //constructor
 	{
 		this.path=path;
 	}
 	
-	public String getText() throws IOException
+	public String getText() throws IOException //this reads file's content
 	{
 		String content="";
-		FileInputStream fstream = new FileInputStream(path);
-		DataInputStream fileIn = new DataInputStream(fstream);
-		BufferedReader buff = new BufferedReader(new InputStreamReader(fileIn));
+		FileInputStream fstream = new FileInputStream(path); //reads file from path
+		DataInputStream fileIn = new DataInputStream(fstream); //reads file's content
+		BufferedReader buff = new BufferedReader(new InputStreamReader(fileIn)); //reads file's characters
 		String lineIn;
+		
 		while ((lineIn=buff.readLine())!=null)
 		{
 			content += (lineIn + "\n");

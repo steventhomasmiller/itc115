1730cac447cc3c24373d1e7b4b713c1eb76381fa
steventhomasmiller <steventhomasmiller@gmail.com>
2015-02-24 09:00:34 -0800

    w/documentation

--- Assignment5/Program.java
+++ Assignment5/Program.java
@@ -3,11 +3,8 @@ package com.spconger.Assignment5;
 public class Program 
 
 {
-
 	public static void main(String[] args) 
 	{
-		Display d = new Display();
-
+		Display d = new Display(); //calls the entire thing
 	}
-
 }

37a4b36b492faf8b35b7c0d7618b0db82868310a
steventhomasmiller <steventhomasmiller@gmail.com>
2015-02-24 08:23:25 -0800

    w/documentation

--- Assignment5/WriteFile.java
+++ Assignment5/WriteFile.java
@@ -6,27 +6,28 @@ import java.io.PrintWriter;
 
 public class WriteFile 
 {
-	/* This class establishes the path to the file
+	/* This class allows the user to add text to a file.
 	Steve Miller, 2/10/15
 	*/
 	
 	private String path;
 	private PrintWriter writer;
 	
-	public WriteFile(String path) throws IOException
+	public WriteFile(String path) throws IOException //constructor
 	{
+		this.path = path;
 		FileWriter outFile = new FileWriter(path, true);
 		writer = new PrintWriter(outFile);
 	}
 	
 	public void addText(String content)
 	{
-		writer.println(content);
+		writer.println(content);//adds line to file
 	}
 	
 	public void closeFile()
 	{
-		writer.close();
+		writer.close();//closes writer
 	}
-	
+
 }

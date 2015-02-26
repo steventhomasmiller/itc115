d3e0e0d9aae3879c0d6b81fd9722932f4088563f
steventhomasmiller <steventhomasmiller@gmail.com>
2015-02-25 17:48:18 -0800

    Update and rename Assignment6 to Assignment6/Recipes.java

---
+++ Assignment6/Recipes.java
@@ -0,0 +1,13 @@
+package com.spconger.Assignment6;
+
+import java.util.ArrayList;
+
+public class Recipes 
+{
+	private ArrayList<Item> recipes;
+	
+	public Recipes()
+	{
+		recipes = new ArrayList<Item>();
+	}
+}

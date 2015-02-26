9aadaff17dfb58df1f3ba4c44654588eddb9d675
steventhomasmiller <steventhomasmiller@gmail.com>
2015-02-25 17:25:39 -0800

    Create Item.java

---
+++ Assignment6/Item.java
@@ -0,0 +1,17 @@
+package com.spconger.Assignment6;
+
+public abstract class Item 
+{
+	private String name;
+
+	public String getName() 
+	{
+		return name;
+	}
+
+	public void setName(String name) 
+	{
+		this.name = name;
+	}
+	
+}

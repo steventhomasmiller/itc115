5d79ee622882721ebdcffef5c8c9d8fddd722d70
steventhomasmiller <steventhomasmiller@gmail.com>
2015-03-04 17:41:21 -0800

    Create Test.java

---
+++ ACMTest/Test.java
@@ -0,0 +1,13 @@
+package com.spconger.ACMTest;
+
+import acm.graphics.*;
+import acm.program.*;
+
+public class Test extends GraphicsProgram
+{
+	public void run() 
+	{
+		GLabel label = new GLabel("Hellay", 50, 60);
+		add(label);
+	}
+}

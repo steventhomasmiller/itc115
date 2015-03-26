766b25e9f7dafc354174e4cae177770c30da1a27
steventhomasmiller <steventhomasmiller@gmail.com>
2015-03-26 11:35:45 -0700

    Update Program.java

--- Assignment6/Program.java
+++ Assignment6/Program.java
@@ -1,5 +1,10 @@
 package com.spconger.Assignment6;
 
-public class Program()
+public class Program
 {
+	public static void main(String[] args)
+	{
+		Recipes recipes = new Recipes();
+		NewRecipe nr = new NewRecipe();
+	}
 }

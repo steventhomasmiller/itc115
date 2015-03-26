0f91cffab93eb557c3ec6653446918162655abf2
steventhomasmiller <steventhomasmiller@gmail.com>
2015-03-25 21:50:56 -0700

    Create Program.java

---
+++ Quiz3/Program.java
@@ -0,0 +1,15 @@
+package Quiz3;
+
+public class Program 
+{
+	/*
+	 * This runs the whole thing
+	 * Steve Miller, 3/25/15
+	 */
+
+	public static void main(String[] args) 
+	{
+		Form f = new Form();
+	}
+
+}

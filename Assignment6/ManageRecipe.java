4f6f656391c611a7a7f29e4e83b85e36f0803641
steventhomasmiller <steventhomasmiller@gmail.com>
2015-03-26 13:32:10 -0700

    Update ManageRecipe.java

--- Assignment6/ManageRecipe.java
+++ Assignment6/ManageRecipe.java
@@ -2,6 +2,8 @@ package com.spconger.Assignment6;
 
 public interface ManageItems
 {
+	/* This class descirbes the methods used in the other classes */
+	
 	void addItem(Item i);
 	void editItem(Item i);
 	void removeItem(Item i);
